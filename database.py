d571bbde776da7c7f079ad395f601ed61175838a
Paruj Ratanaworabhan <paruj.r@gmail.com>
2023-11-17 11:30:32 +0700

    Starting files for the final project for 219114/115.

---
+++ database.py
@@ -0,0 +1,19 @@
+# try wrapping the code below that reads a persons.csv file in a class and make it more general such that it can read in any csv file
+
+import csv, os
+
+__location__ = os.path.realpath(
+    os.path.join(os.getcwd(), os.path.dirname(__file__)))
+
+persons = []
+with open(os.path.join(__location__, 'persons.csv')) as f:
+    rows = csv.DictReader(f)
+    for r in rows:
+        persons.append(dict(r))
+print(persons)
+
+# add in code for a Database class
+
+# add in code for a Table class
+
+# modify the code in the Table class so that it supports the insert operation where an entry can be added to a list of dictionary
---
+++ project_manage.py
@@ -0,0 +1,30 @@
+# import database module
+
+# start by adding the admin related code
+
+# create an object to read an input csv file, persons.csv
+
+# create a 'persons' table
+
+# add the 'persons' table into the database
+
+# create a 'login' table
+
+# the 'login' table has the following keys (attributes):
+
+# person_id
+# username
+# password
+# role
+
+# a person_id is the same as that in the 'persons' table
+# let a username be a person's fisrt name followed by a dot and the first letter of that person's last name
+# let a password be a random four digits string
+# let the initial role of all the students be Member
+# let the initial role of all the faculties be Faculty
+
+# you create a login table by performing a series of insert operations; each insert adds a dictionary to a list
+
+# add the 'login' table into the database
+
+# add code that performs a login task; asking a user for a username and password; returning [person_id, role] if valid, otherwise returning None
